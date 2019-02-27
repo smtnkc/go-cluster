6b6f430adc96c4462f928ee6ea41d973e6d86fc1
Samet Tenekeci <smtnkc@gmail.com>
2019-02-27 10:21:59 +0300

    increased precision of mean BHI scores in validation stats

--- validation_stats.R
+++ validation_stats.R
@@ -10,7 +10,7 @@ o.means.ALL <- data.frame(rbind(
 o.means.ALL$clustering <- c(rep("MCL", 3), rep("SPICi", 3))
 colnames(o.means.ALL) <- c("ontology", "Mean_BHI", "clustering")
 o.means.ALL <- o.means.ALL[,c("clustering", "ontology", "Mean_BHI")]
-o.means.ALL$Mean_BHI <- round(o.means.ALL$Mean_BHI, 3)
+o.means.ALL$Mean_BHI <- round(o.means.ALL$Mean_BHI, 5)
 
 ##### Means of measures
 
@@ -21,7 +21,7 @@ m.means.ALL <- data.frame(rbind(
 m.means.ALL$clustering <- c(rep("MCL", 5), rep("SPICi", 5))
 colnames(m.means.ALL) <- c("measure", "Mean_BHI", "clustering")
 m.means.ALL <- m.means.ALL[,c("clustering", "measure", "Mean_BHI")]
-m.means.ALL$Mean_BHI <- round(m.means.ALL$Mean_BHI, 3)
+m.means.ALL$Mean_BHI <- round(m.means.ALL$Mean_BHI, 5)
 
 ###### Means of topologies
 
@@ -32,7 +32,7 @@ t.means.ALL <- data.frame(rbind(
 t.means.ALL$clustering <- c(rep("MCL", 2), rep("SPICi", 2))
 colnames(t.means.ALL) <- c("topology", "Mean_BHI", "clustering")
 t.means.ALL <- t.means.ALL[,c("clustering", "topology", "Mean_BHI")]
-t.means.ALL$Mean_BHI <- round(t.means.ALL$Mean_BHI, 3)
+t.means.ALL$Mean_BHI <- round(t.means.ALL$Mean_BHI, 5)
 
 ###### Write mean stats
 
